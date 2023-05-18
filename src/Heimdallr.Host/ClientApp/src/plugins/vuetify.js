41a8d3fd9e969bddb5b3a4e111880809c62d4bab
Клабуков Эрик Константинович <klabukov@skbkontur.ru>
2023-05-18 19:24:59 +0500

    Install mdi/fonts and remove download css from cdn (is so slowly)

--- src/Heimdallr.Host/ClientApp/src/plugins/vuetify.js
+++ src/Heimdallr.Host/ClientApp/src/plugins/vuetify.js
@@ -1,7 +1,11 @@
 import Vue from 'vue';
 import Vuetify from 'vuetify/lib/framework';
+import "@mdi/font/css/materialdesignicons.min.css";
 
 Vue.use(Vuetify);
 
 export default new Vuetify({
+    icons: {
+        iconFont: "md",
+    },
 });
